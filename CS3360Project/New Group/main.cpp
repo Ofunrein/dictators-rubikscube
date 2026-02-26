952577b6015f9772c12de995627d85f448acad1c
KTBarnette <vgv28@txstate.edu>
2026-02-26 11:21:44 -0600

    Hello, world
    contributor-cache-refresh

--- CS3360Project/New Group/main.cpp
+++ CS3360Project/New Group/main.cpp
@@ -4,5 +4,12 @@
 //
 //  Created by Kyle T. Barnette on 2/24/26.
 //
-
+#include <iostream>
 #include "main.hpp"
+using namespace std;
+int main(){
+    cout << "hello, world" << endl;
+    
+    return 0;
+}
+
